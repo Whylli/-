dd871ab00315c9de3783f50c7f2309fbed2ad271
Whylli <stepanmarunich03@gmail.com>
2025-01-31 15:46:21 +0100

    Delete l9_imports directory

--- l9_imports/task3/task3/task3/task3.py
+++
@@ -1,7 +0,0 @@
-# Import the function `multiply` from `l2_functions` module
-# to make the script work without errors
-def multiply(a: int, b: int) -> int:
-    return a * b
-# Do not modify the code below
-if __name__ == "__main__":
-    assert multiply(2, 2) == 4
